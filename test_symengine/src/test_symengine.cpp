9926b8c3f67b269c86a8989fbac93dea52c87e26
liuwenxue <andy.liu@zichuan-tech.com>
2024-07-10 13:20:27 +0800

    测试sym::Rot3的用法

--- test_symengine/src/test_symengine.cpp
+++ test_symengine/src/test_symengine.cpp
@@ -5,5 +5,10 @@
 int main(int argc, char* argv[])
 {
     std::cout << sym::Pose3d() << std::endl;
+    std::cout << sym::Rot3<double>() << std::endl;
+
+    sym::Rot3<double> Q = sym::Rot3<double>();
+    const Eigen::Matrix<double, 4, 1>& rotation = Q.Data();
+    std::cout << "rotation=" << rotation.transpose() << std::endl;
     return 0;
 }
